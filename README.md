31053e9d88432b1dc8c8485c625fd326e050fda2
josue@punchteam.com <m@josueochoa.com>
2021-01-01 15:30:58 -0500

    merge

---
+++ README.md
@@ -0,0 +1,2 @@
+# Punch Year
+Simple plugin to output the current year, ideal for site footer contents, usage: `[year]`
---
+++ year-shortcode.php
@@ -0,0 +1,16 @@
+<?php
+/*
+Plugin Name: Year Shortcode
+Plugin URI: http://www.punchteam.com
+Description: A shortcode to output the current year, usage: [year]
+Version: 1.0
+Author: Punchteam
+Author URI: http://www.punchteam.com
+*/
+
+
+add_shortcode( 'year', 'punch_year');
+
+function punch_year($attr) {
+    return date("Y");
+}
